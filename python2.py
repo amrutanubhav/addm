dd625473e72fd73399ffa5f8354d28106eca4ca0
Amrut Anubhav <amrut.anubhav@ericsson.com>
2020-08-20 10:42:08 +0530

    code modified

--- python2.py
+++ python2.py
@@ -1,6 +1,9 @@
-a = 10
-b = 20
-c = a+b
-print(c)
-print("hello amrut")
+print("GOOD MORNING")
+my_text = 'Amrut Anubhav, Agraj Khanna, Shubhangi Gaur'
+test = my_text.split(' ')
+print(test[0])
+print(test[2])
+print(test[4])
+
+
 
